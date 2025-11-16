e240909c8e5a93c9664a650db89026b9bc8cbeb2
Nikos Koumbakis <spiralout.eu@gmail.com>
2025-11-16 12:28:48 +0200

    Update object IDs in market creation and pseudo enclave registration scripts

--- create_market_ptb.sh
+++ create_market_ptb.sh
@@ -17,9 +17,9 @@ PRICE=0
 RESULT=0
 
 # Object IDs
-DELPHI_CONFIG_OBJECT_ID="0xb84d0750044939d717d1520f04dc8f3c03556f5d1181f3f7ca592a8aa72f8e50"  # Replace with your Config object ID (shared object created during init)
-ENCLAVE_OBJECT_ID="0xda57c3204986540b5e680b80ff2bb8b83db5a0e50a2cd581f2399bf0898a9ba3"
-DELPHI_PACKAGE_ID="0x75f47435691bc728cefaae4bb7c7a1abb22c806e602cde50f2081e69a6c6169d"
+DELPHI_CONFIG_OBJECT_ID="0x9de4688eac02dc49466582c94b11df31de86a02cec66284fe48d9a0e74e099b9"  # Replace with your Config object ID (shared object created during init)
+ENCLAVE_OBJECT_ID="0xdd4141ea815f54a500d0136722a6d1bf56825d8a883fc59a3a93876fa5de506a"
+DELPHI_PACKAGE_ID="0xf5622bf11bac2c0653f6da28e53263b45e7ac2cf5f493cc53ef7fe0f3734012c"
 
 # Remove 0x prefix for conversion
 SIG_HEX_NO_PREFIX="${SIGNATURE_HEX#0x}"
