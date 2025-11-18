a2b9bcd4d8bd360519d3a643263594067373d022
Nikos Koumbakis <spiralout.eu@gmail.com>
2025-11-18 22:21:56 +0200

    Remove AdminPage and related routes from App component.

--- delphi-frontend/src/App.tsx
+++ delphi-frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Landing } from "./pages/Landing";
 import { MyPositionsPage } from "./pages/MyPositionsPage";
-import { AdminPage } from "./pages/AdminPage";
 import { CreateMarketPage } from "./pages/CreateMarketPage";
 import { ExplorePage } from "./pages/ExplorePage";
 import { MarketDetailPage } from "./pages/MarketDetailPage";
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/explore" element={<ExplorePage />} />
         <Route path="/positions" element={<MyPositionsPage />} />
-        <Route path="/admin" element={<AdminPage />} />
         <Route path="/create-market" element={<CreateMarketPage />} />
         <Route path="/market/:marketId" element={<MarketDetailPage />} />
       </Routes>
